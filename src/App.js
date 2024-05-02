5580b8feee2286f82de43da7905a32109b4a35f9
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-05-02 14:49:00 +0500

    S14 L3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React from "react";
+
+class App extends React.Component {
 
 
-export default function App() {
+  constructor(props) {
+    super(props) ;
 
-  return(
-    <div>
-      hello
-    </div>
-  )
+    this.state = { count : 5} ;
+
+    this.handle_btn_decrement_click = this.handle_btn_decrement_click.bind(this) ;
+    this.handle_btn_increment_click = this.handle_btn_increment_click.bind(this) ;
+
+  }
+
+  handle_btn_decrement_click() {
+    this.setState( (curr_state) => {
+      return{ count : curr_state.count-1} ;
+    })
+  }
+
+  handle_btn_increment_click() {
+    this.setState( (curr_state) => {
+      return{ count : curr_state.count+1} ;
+    })
+    
+  }
+
+
+  render() {
+
+    const date = new Date("june 10 2024") ;
+    date.setDate(date.getDate() + this.state.count) ;
+
+    return(
+
+      <div className="div_containing_everything">
+        <button className="btn_decrement" onClick={this.handle_btn_decrement_click}>-</button>
+            <p> {date.toDateString()} [{this.state.count}]</p>
+        <button className="btn_increment" onClick={this.handle_btn_increment_click}>+</button>
+      </div>
+    )
+  }
+
+
 }
+
+export default App ;
